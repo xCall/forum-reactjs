9503d83044ba9eb4903adf3adfa30311a6607cc5
xCall <carlosfaita99@gmail.com>
2022-11-19 12:43:15 -0300

    finalizado comentarios

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,5 +1,5 @@
-import { ThumbsUp, Trash } from 'phosphor-react';
-import styles from './Comment.module.css';
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
 
 export function Comment() {
   return (
@@ -10,17 +10,26 @@ export function Comment() {
         <div className={styles.commentContent}>
           <header>
             <div className={styles.authorAndTime}>
-              <time title="11 de maio públicado as 08:13h" dateTime="2022-05-11 08 08:13:23">Cerca de 1h atŕas</time>
+              <strong>Carlos Faita</strong>
+              <time
+                title="11 de maio públicado as 08:13h"
+                dateTime="2022-05-11 08 08:13:23"
+              >
+                Cerca de 1h atŕas
+              </time>
             </div>
-            <button title='Deletar comentário'>
-              <Trash size={20}/>
+            <button title="Deletar comentário">
+              <Trash size={24} />
             </button>
           </header>
           <p>Muito bom Devon, parabéns!!</p>
         </div>
         <footer>
-          <ThumbsUp size={20} />
-          Aplaudir <span></span>
+          <button>
+            {" "}
+            <ThumbsUp size={20} />
+            Aplaudir <span>20</span>
+          </button>
         </footer>
       </div>
     </div>
