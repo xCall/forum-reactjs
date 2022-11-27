ae880fb0af054f7511584e309d6b97f6823676aa
xCall <carlosfaita99@gmail.com>
2022-11-27 14:29:45 -0300

    adicionado typagem ts

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App'
 
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
